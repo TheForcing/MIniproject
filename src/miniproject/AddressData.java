a2313222b1e1099af6f62a5a8152f8c1e54fce61
박지훈 <qkrw6@naver.com>
2021-08-10 01:56:15 +0900

    미니프로젝트

--- src/miniproject/AddressData.java
+++ src/miniproject/AddressData.java
@@ -5,17 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class AddressData {
-	public String name;
-	public String hp;
-	public String tel;
-    BufferedReader in= new BufferedReader(new InputStreamReader(System.in));
-   public AddressData() throws IOException{
-	   System.out.print("이름:");
-	   name=in.readLine();
-	   System.out.print("핸드폰번호:");
-	   hp=in.readLine();
-	   System.out.print("전화번호:");
-	   tel=in.readLine();
+	  String name;
+      String hp;
+      String tel;
+      public AddressData(String name, String hp, String tel) {
+      	this.name=name;
+      	this.hp=hp;
+      	this.tel=tel;
 	   
    }
    
